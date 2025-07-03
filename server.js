26c41d679def7579e9e86618285b74093ddbeffd
fhalo05 <spicerbob020@gmail.com>
2025-07-03 17:50:57 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,73 +1,47 @@
-const localVideo = document.getElementById('localVideo');
-const remoteVideo = document.getElementById('remoteVideo');
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
 
-let localStream;
-let peerConnection;
+const app = express();
+const server = http.createServer(app);
 
-const servers = {
-  iceServers: [
-    { urls: 'stun:stun.l.google.com:19302' }
-  ]
-};
-
-async function startCamera() {
-  try {
-    localStream = await navigator.mediaDevices.getUserMedia({ video: true, audio: true });
-    localVideo.srcObject = localStream;
-  } catch (err) {
-    alert('Could not get camera/microphone: ' + err);
-    console.error(err);
+const io = new Server(server, {
+  cors: {
+    origin: '*',
+    methods: ['GET', 'POST']
   }
-}
+});
 
-startCamera();
+app.use(express.static('public'));
 
-socket.on('partner-found', async (partnerId) => {
-  peerConnection = new RTCPeerConnection(servers);
+let waitingUser = null;
 
-  localStream.getTracks().forEach(track => {
-    peerConnection.addTrack(track, localStream);
+io.on('connection', (socket) => {
+  console.log('User connected:', socket.id);
+
+  if (waitingUser) {
+    const partner = waitingUser;
+    waitingUser = null;
+
+    partner.emit('partner-found', socket.id);
+    socket.emit('partner-found', partner.id);
+  } else {
+    waitingUser = socket;
+  }
+
+  socket.on('signal', ({ to, data }) => {
+    io.to(to).emit('signal', { from: socket.id, data });
   });
 
-  peerConnection.ontrack = event => {
-    remoteVideo.srcObject = event.streams[0];
-  };
-
-  peerConnection.onicecandidate = event => {
-    if (event.candidate) {
-      socket.emit('signal', { to: partnerId, data: { candidate: event.candidate } });
+  socket.on('disconnect', () => {
+    if (waitingUser && waitingUser.id === socket.id) {
+      waitingUser = null;
     }
-  };
-
-  // To avoid both peers creating offers simultaneously:
-  if (socket.id < partnerId) {
-    const offer = await peerConnection.createOffer();
-    await peerConnection.setLocalDescription(offer);
-    socket.emit('signal', { to: partnerId, data: { offer } });
-  }
+    socket.broadcast.emit('partner-disconnected', socket.id);
+  });
 });
 
-socket.on('signal', async ({ from, data }) => {
-  if (data.offer) {
-    await peerConnection.setRemoteDescription(new RTCSessionDescription(data.offer));
-    const answer = await peerConnection.createAnswer();
-    await peerConnection.setLocalDescription(answer);
-    socket.emit('signal', { to: from, data: { answer } });
-  } else if (data.answer) {
-    await peerConnection.setRemoteDescription(new RTCSessionDescription(data.answer));
-  } else if (data.candidate) {
-    try {
-      await peerConnection.addIceCandidate(new RTCIceCandidate(data.candidate));
-    } catch (e) {
-      console.error('Error adding ICE candidate:', e);
-    }
-  }
-});
-
-socket.on('partner-disconnected', () => {
-  if (peerConnection) {
-    peerConnection.close();
-    peerConnection = null;
-  }
-  remoteVideo.srcObject = null;
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
